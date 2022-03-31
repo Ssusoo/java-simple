90bd2f037e3e232a99cb3518b741ee90346f4385
김수환 <zzanggoon8@hiconsy.com>
2022-03-31 19:37:07 +0900

    first commit

---
+++ src/main/java/me/ssu/javasimple/JavaSimpleApplication.java
@@ -0,0 +1,13 @@
+package me.ssu.javasimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSimpleApplication.class, args);
+	}
+
+}
